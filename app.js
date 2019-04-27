b3d518a76025154800740311beca10e036c4566a
wilson daniel martinez <imwiwiim90@wilsons-MacBook-Air.local>
2019-04-27 09:10:43 -0500

    tring to fix 304;

--- app.js
+++ app.js
@@ -67,6 +67,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+// 304 problem
+app.disable('etag');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
